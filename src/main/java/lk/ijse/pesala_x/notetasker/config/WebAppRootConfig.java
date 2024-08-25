8beb0f3980b85a9eea8f0962d32666e700b531fb
pesala-x <gkpesalawinodith@gmail.com>
2024-08-25 15:44:49 +0530

    add configs for Driver Manager Datasource

--- src/main/java/lk/ijse/pesala_x/notetasker/config/WebAppRootConfig.java
+++ src/main/java/lk/ijse/pesala_x/notetasker/config/WebAppRootConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -24,9 +25,12 @@ import javax.sql.DataSource;
 public class WebAppRootConfig {
     @Bean
     public DataSource dataSource() {
-
-        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        return builder.setType(EmbeddedDatabaseType.HSQL).build();
+        var dmds = new DriverManagerDataSource();
+        dmds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dmds.setUrl("jdbc:mysql://localhost:3306/noteTakerAAD68?createDatabaseIfNotExist=true");
+        dmds.setUsername("root");
+        dmds.setPassword("Ijse@1234");
+        return dmds;
     }
 
     @Bean
