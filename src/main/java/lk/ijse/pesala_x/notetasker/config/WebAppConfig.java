d62f0724fd38f36e46d871a4643c400fb643bfa1
pesala-x <gkpesalawinodith@gmail.com>
2024-09-02 23:32:11 +0530

    add config in WebAppConfig for multipart form data

--- src/main/java/lk/ijse/pesala_x/notetasker/config/WebAppConfig.java
+++ src/main/java/lk/ijse/pesala_x/notetasker/config/WebAppConfig.java
@@ -1,5 +1,6 @@
 package lk.ijse.pesala_x.notetasker.config;
 
+import jakarta.servlet.annotation.MultipartConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,4 +12,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @EnableJpaRepositories(basePackages = "lk.ijse.pesala_x.notetasker")
 @EnableTransactionManagement
+@MultipartConfig (
+        fileSizeThreshold = 1024 * 1024 * 2, //2mb
+        maxFileSize = 1024 * 1024 * 10, //10mb
+        maxRequestSize = 1024 * 1024 * 10 // 10mb
+)
 public class WebAppConfig {}
